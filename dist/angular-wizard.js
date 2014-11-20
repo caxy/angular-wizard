387436c1614ae70f170305587f6fc9de61338e17
Mike <mtrakan@caxy.com>
2014-11-20 15:26:25 -0600

    added SIFMA 'return to start' button to wizard

--- dist/angular-wizard.js
+++ dist/angular-wizard.js
@@ -16,7 +16,10 @@ angular.module("wizard.html", []).run(["$templateCache", function($templateCache
   $templateCache.put("wizard.html",
     "<div>\n" +
     "    <div class=\"steps\" ng-transclude></div>\n" +
-    "    <ul class=\"steps-indicator steps-{{steps.length}}\" ng-if=\"!hideIndicators\">\n" +
+    "    <ul class=\"steps-indicator steps-{{steps.length+1}}\" ng-if=\"!hideIndicators\">\n" +
+    "      <li class=\"done\">\n" +
+    "        <a href=\"/#/start\">Start</a>\n" +
+    "      </li>\n" +
     "      <li ng-class=\"{default: !step.completed && !step.selected && !step.reached, current: step.selected && !step.completed, done: step.completed && !step.selected, reached: step.reached && !step.selected, editing: step.selected && step.completed}\" ng-repeat=\"step in steps\">\n" +
     "        <a ng-click=\"goTo(step)\">{{step.title || step.wzTitle}}</a>\n" +
     "      </li>\n" +
